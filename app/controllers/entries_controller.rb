d18325343834259666978ef0acf537da1679f2da
Sami <Sami@Samanthas-MacBook-Pro.local>
2016-07-06 19:06:58 +1000

    Include edit capability

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -11,8 +11,19 @@ class EntriesController < ApplicationController
   end
 
   def create
-     entry_params = params["entry"].permit("title", "contents")
-     entry = Entry.create(entry_params)
-     redirect_to(entry_path(entry))
+    entry_params = params["entry"].permit("title", "contents")
+    entry = Entry.create(entry_params)
+    redirect_to(entry_path(entry))
+  end
+
+  def edit
+    @entry = Entry.find(params["id"])
+  end
+
+  def update
+    entry_params = params["entry"].permit("title", "contents")
+    entry = Entry.find(params["id"])
+    entry.update(entry_params)
+    redirect_to(entry_path(entry))
   end
 end
